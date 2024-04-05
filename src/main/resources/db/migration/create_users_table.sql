13dfa46050c51e4f1273271707c158779ba7eb2f
MegaDen58 <96517069+MegaDen58@users.noreply.github.com>
2024-04-05 21:03:15 +0300

    Update create_users_table.sql

--- src/main/resources/db/migration/create_users_table.sql
+++ src/main/resources/db/migration/create_users_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     login VARCHAR(50) NOT NULL,
-    password VARCHAR(100) NOT NULL
+    password VARCHAR(100) NOT NULL,
+    email varchar(150) NOT NULL
 );
