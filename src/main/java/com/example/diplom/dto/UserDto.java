2ec8c4733302eee3a0dc17ae78e6aa812b751f39
MegaDen58 <96517069+MegaDen58@users.noreply.github.com>
2024-04-07 13:48:35 +0300

    Update UserDto.java

--- src/main/java/com/example/diplom/dto/UserDto.java
+++ src/main/java/com/example/diplom/dto/UserDto.java
@@ -1,21 +1,11 @@
 package com.example.diplom.dto;
 
 import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserDto {
     private Long id;
     private String login;
     private String password;
     private String email;
-
-    public UserDto(String login, String password){
-        this.login = login;
-        this.password = password;
-    }
 }
