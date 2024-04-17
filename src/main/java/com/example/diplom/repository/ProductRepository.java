24313ec24717a29ef7278fbdb73151a0572b694f
Egor1238349349374 <iolitovich122@gmail.com>
2024-04-17 22:11:51 +0300

    поиск продукта по картинке

--- src/main/java/com/example/diplom/repository/ProductRepository.java
+++ src/main/java/com/example/diplom/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.example.diplom.repository;
 
-import com.example.diplom.dto.ProductDto;
 import com.example.diplom.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Product findProductById(int id);
+    @Query("SELECT p FROM Product p WHERE :image MEMBER OF p.images")
+    Product findProductByImage(String image);
 }
