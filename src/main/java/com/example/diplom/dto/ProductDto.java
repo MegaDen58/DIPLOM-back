e031ad160e45e54f4907d7514cc827aa12af2040
MegaDen58 <extreme.den2004@gmail.com>
2024-05-12 22:22:46 +0300

    новые поля в товаре (размер, цвет, материал)

--- src/main/java/com/example/diplom/dto/ProductDto.java
+++ src/main/java/com/example/diplom/dto/ProductDto.java
@@ -21,7 +21,6 @@ public class ProductDto {
     private String color;
     private String material;
     private String size;
-
     @Override
     public String toString() {
         return "ProductDto{" +
