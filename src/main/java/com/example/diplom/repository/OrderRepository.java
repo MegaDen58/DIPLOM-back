cd74d99c9eba761cd44c4b71c54a8fa23b6e7e41
MegaDen58 <extreme.den2004@gmail.com>
2024-05-16 11:59:15 +0300

    Поиск инвентаря по ид заказа

--- src/main/java/com/example/diplom/repository/OrderRepository.java
+++ src/main/java/com/example/diplom/repository/OrderRepository.java
@@ -15,6 +15,6 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("SELECT o FROM Order o WHERE o.user_id = :userId")
     List<Order> findByUser_id(Long userId);
 
-    @Query("SELECT p FROM products p JOIN order_items oi ON p.id = oi.items WHERE oi.order_id = :orderId")
+    @Query("SELECT p FROM Product p JOIN OrderItem oi ON p.id = oi.items WHERE oi.orderId = :orderId")
     List<Product> findProductsByOrderId(Long orderId);
 }
